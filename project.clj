e58afedb6ea4231a49c35f01042ea08662dee955
James Reeves <jreeves@weavejester.com>
2012-04-13 19:38:48 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject indolent "0.1.0-SNAPSHOT"
+  :description "Restful JSON HTTP client"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clj-http "0.3.5"]])
---
+++ test/indolent/test/client.clj
@@ -0,0 +1,7 @@
+(ns indolent.test.client
+  (:use clojure.test
+        indolent.client))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
