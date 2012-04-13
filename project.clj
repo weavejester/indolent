f752174c4b120e5d7057850ca8a7b9204f47face
James Reeves <jreeves@weavejester.com>
2012-04-13 22:33:22 +0100

    Only return the body of the response

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/core.incubator "0.1.0"]
                  [clj-http "0.3.5"]
                  [slingshot "0.10.2"]
                  [cheshire "4.0.0"]])
--- src/indolent/client.clj
+++ src/indolent/client.clj
@@ -1,9 +1,11 @@
 (ns indolent.client
   (:refer-clojure :exclude [get])
   (:require [clj-http.client :as http])
-  (:use [slingshot.slingshot :only [try+]]))
+  (:use [slingshot.slingshot :only [try+]]
+        [clojure.core.incubator :only [-?>]]))
 
 (defn get [[root & path]]
   (try+
-   (http/get root {:as :json})
+   (-?> (http/get root {:as :json})
+        :body)
    (catch [:status 404] _ nil)))
