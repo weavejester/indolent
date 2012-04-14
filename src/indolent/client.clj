6660e616dba777ddae147f054e06b4039b0723f4
James Reeves <jreeves@weavejester.com>
2012-04-14 13:11:16 +0100

    Build URL up from smaller pieces

--- src/indolent/client.clj
+++ src/indolent/client.clj
@@ -1,11 +1,22 @@
 (ns indolent.client
   (:refer-clojure :exclude [get])
-  (:require [clj-http.client :as http])
+  (:import java.net.URLEncoder)
+  (:require [clj-http.client :as http]
+            [clojure.string :as str])
   (:use [slingshot.slingshot :only [try+]]
         [clojure.core.incubator :only [-?>]]))
 
-(defn get [[root & path]]
+(defn- url-encode [s]
+  (URLEncoder/encode s "UTF-8"))
+
+(defn- make-url [[root & paths]]
+  (->> (map url-encode paths)
+       (str/join "/")
+       (str root "/")))
+
+(defn get [url]
   (try+
-   (-?> (http/get root {:as :json})
+   (-?> (make-url url)
+        (http/get {:as :json})
         :body)
    (catch [:status 404] _ nil)))
